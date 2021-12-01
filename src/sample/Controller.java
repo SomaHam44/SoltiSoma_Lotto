9fb8a3465bc1345739b6712d319c19cf4ee77cdd
Your Name <somasolti@gmail.com>
2021-12-01 16:40:45 +0100

    Controllerben elemek hozzáadása

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,20 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class Controller {
+
+    @FXML
+    public Button btnSorsolas;
+    @FXML
+    public ListView listHuzottSzamok;
+    @FXML
+    public Label lblSorsoltSzam;
+
 
     public void SorsolasClick(ActionEvent actionEvent) {
     }
