1de39fe426e4d1da0d36e28afcd3bd26bf1f5e44
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-06 17:15:21 +0100

    Javítás, újítás, futtatási próbák

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,19 +12,22 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 public class Controller {
+
     private List<Integer> sorsoltSzamokListaja = new ArrayList<>();
     private int kisorsoltSzam;
     private int kihuzottakSzama;
-
-
+    private int elsoSzam, masodikSzam, harmadikSzam, negyedikSzam, otodikSzam;
 
     @FXML
     public Button btnSorsolasRendezes;
     @FXML
-    public ListView listHuzottSzamok;
-    @FXML
     public Label lblSorsoltSzam;
-
+    @FXML
+    public Label kisorsoltszam1;
+    public Label kisorsoltszam2;
+    public Label kisorsoltszam3;
+    public Label kisorsoltszam4;
+    public Label kisorsoltszam5;
     private Timer timer;
 
     public void init() {
@@ -33,6 +36,10 @@ public class Controller {
 
 
     public void SorsolasClick(ActionEvent actionEvent) {
+        lblSorsoltSzam.setText("" + sorsolosVeletlenSzamGenerator());
+        sorsol();
+
+        veletlensorsolo();
         if (kihuzottakSzama > 0 && kihuzottakSzama < 6)
         sorsol();
         else if (kihuzottakSzama == 5) {
@@ -41,12 +48,21 @@ public class Controller {
 
     }
 
+    private int sorsolosVeletlenSzamGenerator() {
+        for (int i = 0; i < 91; i++) {
+            sorsoltSzamokListaja.add(i);
+        }
+
+        kisorsoltSzam = (int) (Math.random() * sorsoltSzamokListaja.size() - 1) + 1;
+        return kisorsoltSzam;
+    }
+
     private void sorsol() {
         for (int i = 0; i < 91; i++) {
             sorsoltSzamokListaja.add(i);
         }
 
-        kisorsoltSzam = (int) Math.random() * sorsoltSzamokListaja.size() - 1;
+        kisorsoltSzam = (int) (Math.random() * sorsoltSzamokListaja.size() - 1) + 1;
         timer = new Timer();
         TimerTask timerTask = new TimerTask() {
             @Override
@@ -57,12 +73,14 @@ public class Controller {
             }
 
         };
-        timer.schedule(timerTask, 1, 1);
+        timer.schedule(timerTask, 2000);
+    }
 
-
-
-
-
+    private void veletlensorsolo() {
+        if (kihuzottakSzama == 0) {
+            kihuzottakSzama++;
+        }
+        lblSorsoltSzam.setText(" " + kisorsoltSzam);
 
     }
 
